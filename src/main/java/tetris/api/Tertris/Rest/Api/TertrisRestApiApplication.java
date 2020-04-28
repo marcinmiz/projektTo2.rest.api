a08a9c755b6645cedb82f485729f35112c171769
Mateusz <agry123@wp.pl>
2020-04-28 10:01:10 +0200

    Init backend

---
+++ src/main/java/tetris/api/Tertris/Rest/Api/TertrisRestApiApplication.java
@@ -0,0 +1,13 @@
+package tetris.api.Tertris.Rest.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TertrisRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TertrisRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tetris/api/Tertris/Rest/Api/TertrisRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tetris.api.Tertris.Rest.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TertrisRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
