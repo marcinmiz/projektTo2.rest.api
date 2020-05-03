7b49ce1b3ad16bf5255c526240d5ef5e2ca9d45c
Mateusz <agry123@wp.pl>
2020-05-03 16:42:26 +0200

    "Adding initialization mode and (data.sql) some dummy data"

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO security_question (id,question) VALUES (0,'What is your favourite place?');
+INSERT INTO security_question (id,question) VALUES (1,'What is your favourite animal?');
+INSERT INTO security_question (id,question) VALUES (2,'What is your favourite dictator?');
+INSERT INTO security_question (id,question) VALUES (3,'What is your favourite movie?');
+
+-- Wszystkiego najlepszego Marcin --
